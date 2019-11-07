3b42bacf57815bcabe9118bbb52e855cc0dedea1
arcadeforge <jochen.zurborg@gmx.net>
2019-11-07 13:41:28 +0000

    added rotate behaviour for autostart and rotate script

--- home/pi/RetroPie/retropiemenu/rotate screen.sh
+++ home/pi/RetroPie/retropiemenu/rotate screen.sh
@@ -8,17 +8,20 @@
 
 killall emulationstation 2> /dev/null
 
-grep es_rotate /boot/config.txt >> /home/pi/config.sh
+grep es_rotate /boot/config.txt > /home/pi/config.sh
 chmod +x /home/pi/config.sh
 . /home/pi/config.sh
-#rm  /home/pi/config.sh
-echo $es_rotate
 
-new_rotate=$($es_rotate + 1)
-if [ $es_rotate -eq 4 ] ; then
-	new_rotate=0
+#rm  /home/pi/config.sh
+#echo $es_rotate
+
+new_rotate=$(( $es_rotate + 1 ))
+if [ $new_rotate -eq 4 ] ; then
+    new_rotate=0
 fi
 
-echo "starting emulationstation with rotate $es_rotate"
-sleep 2
-emulationstation --screenrotate $es_rotate
+#echo "starting emulationstation with rotate $new_rotate"
+#sleep 1
+sudo sed -i "s/^es_rotate.*/es_rotate=$new_rotate/" /boot/config.txt
+
+emulationstation --screenrotate $new_rotate
