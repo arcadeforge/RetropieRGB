eca5c8655d22e63c39be26a031cf31c1b356d4a4
arcadeforge <jochen.zurborg@gmx.net>
2019-11-07 13:46:55 +0000

    added setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
-echo "Installing now the files for your retropie setup"
+echo "installing files"
 
-sudo cp -r * /
-
-echo "Done."
+cp opt/retropie/configs/all/autostart.sh  /opt/retropie/configs/all/
+cp home/pi/RetroPie/retropiemenu/rotate\ screen.sh  /home/pi/RetroPie/retropiemenu
+echo "cp opt/retropie/configs/all/autostart.sh  /opt/retropie/configs/all/"
+echo "cp home/pi/RetroPie/retropiemenu/rotate\ screen.sh  /home/pi/RetroPie/retropiemenu"
+echo "done."
