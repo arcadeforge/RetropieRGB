13e00686325183ae72ef0c12af2a7ed7a93e2b0e
arcadeforge <jochen.zurborg@gmx.net>
2019-11-08 13:48:06 +0000

    runcoammnd on end inserted

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ cp opt/retropie/configs/all/vertical.txt /opt/retropie/configs/all/
 cp opt/retropie/configs/all/runcommand-onstart.sh /opt/retropie/configs/all/
 cp /opt/retropie/configs/all/runcommand-onstart.sh /opt/retropie/configs/all/runcommand-onstart.sh_asche
 cp opt/retropie/configs/all/arcade_res_table.txt /opt/retropie/configs/all/
+cp opt/retropie/configs/all/runcommand-onend.sh /opt/retropie/configs/all/
 
 more setup.sh
 echo "done."
