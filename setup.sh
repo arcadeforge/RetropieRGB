6a7ac8705f599811c92c0f138fbe90bf851311cf
arcadeforge <jochen.zurborg@gmx.net>
2019-11-08 22:16:58 +0000

    ext setup

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ cp opt/retropie/configs/all/autostart.sh  /opt/retropie/configs/all/
 cp home/pi/RetroPie/retropiemenu/rotate\ screen.sh  /home/pi/RetroPie/retropiemenu
 cp home/pi/rgb_default_config.sh /home/pi/
 sudo cp usr/local/bin/mme4crt /usr/local/bin/
+sudo cp usr/local/bin/pikeyd165 /usr/local/bin/
 cp opt/retropie/configs/all/vertical.txt /opt/retropie/configs/all/
 cp opt/retropie/configs/all/runcommand-onstart.sh /opt/retropie/configs/all/
 cp /opt/retropie/configs/all/runcommand-onstart.sh /opt/retropie/configs/all/runcommand-onstart.sh_asche
