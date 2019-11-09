a35fd1a65dc5947d72e48642daf0a64fe6fe3295
arcadeforge <jochen.zurborg@gmx.net>
2019-11-09 11:36:03 +0000

    clean

--- scripts/wget_git.sh
+++
@@ -1,2 +0,0 @@
-echo "wget https://raw.githubusercontent.com/arcadeforge/$1/master/$2"
-wget https://raw.githubusercontent.com/arcadeforge/$1/master/$2
--- scripts/wget_gitblob.sh
+++
@@ -1,2 +0,0 @@
-echo "wget https://raw.githubusercontent.com/arcadeforge/$1/master/$2"
-wget https://github.com/arcadeforge/$1/blob/master/$2
