44c1a80126350df3b39ff67179fba0074d035d45
arcadeforge <jochen.zurborg@gmx.net>
2019-11-09 11:07:07 +0000

    added online patch

--- home/pi/RetroPie/retropiemenu/Update RetroPieRGB.sh
+++ home/pi/RetroPie/retropiemenu/Update RetroPieRGB.sh
@@ -1,4 +1,6 @@
 dir=$(pwd)
 cd /home/pi/RetroPieRGB
 git pull origin master
+chmod +x patch.sh
+./patch.sh
 cd $dir
---
+++ patch.sh
@@ -0,0 +1,2 @@
+dialog --infobox "RetroPieRGB First Version\n\n Visit www.arcadeforge.de" 10 30
+sleep 3
